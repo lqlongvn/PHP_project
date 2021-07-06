2cce1946bb9e40557084d6d3ac281d2f00b71864
Long <lqlongvn@gmail.com>
2021-07-06 20:21:00 +0700

    Bootstrap - container, input text, button, header row; ok

--- index(bootstrap).php
+++ index(bootstrap).php
@@ -1,87 +1,74 @@
-<?php 
-session_start();
-include_once 'config/myConfig.php'; 
-
-echo "Hello Long, this is index.php", "<br>";
-
-echo "id: " . "             - Work: " . "        - Done: " . "    - Date:      " . "<br>";
-
-$sql = "SELECT * FROM work";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-	while($row = $result->fetch_assoc()) {
-		echo "id: " . $row["id"]. " - Work: " . $row["work_content"]. "- Done: " . $row["done_content"]." - Date:  " . $row["work_date"]. "<br>";
-	}
-} else {
-	echo "0 results";
-}
-$conn->close();
-
-?>
-
-
-
-<!DOCTYPE html>
-<html lang="">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Hệ Thống Quản lý Mua Sắm VNPT-Media </title>
 	<?php 
-	echo  "<br>";
-	echo "Hello: GitHub; Just to see the Changes in GitHub; Long sửa để thấy thay đổi trên GitHub", "<br>";
+	session_start();
+	include_once 'config/myConfig.php'; 
+
+	echo "Hello Long, this is index.php", "<br>";
+
+	echo "id: " . "             - Work: " . "        - Done: " . "    - Date:      " . "<br>";
+
+	$sql = "SELECT * FROM work";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["id"]. " - Work: " . $row["work_content"]. "- Done: " . $row["done_content"]." - Date:  " . $row["work_date"]. "<br>";
+		}
+	} else {
+		echo "0 results";
+	}
+	$conn->close();
+
 	?>
 
-	<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="Bootstrap/css/my_css.css">
-</head>
-<body>
 
+	<!DOCTYPE html>
+	<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Quản lý To-do-list </title>
 
-
-	<div class="container">
-		<?php echo "<h3 style='color: red;'> Hello Bootstrap!</h3>"; ?>
-
-	</div>
-
-	<div class="container">
-		<div class="row main">
-			<div class="col-md-4">
-				<div class="alert alert-danger">Quay lại</div>
-				<div class="alert alert-info">Thông tin thêm</div>
+		<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="Bootstrap/css/my_css.css">
+	</head>
+	<body>
+		<div class="container">
+			<h4>DANH SÁCH GÓI THẦU</h4>
+			<div class="row">
+				<div class="col-md-6">
+					<form action="index.php?page=search" method="POST" name="frm_search">
+						<input type="text" class="form-control" name="key" placeholder="Nội dung cần tìm trong Tên Gói thầu" />
+				</div>
+				<div class="col-md-6">
+						<button class="btn btn-primary" type="submit" name="submit">Tìm kiếm theo tên</button>
+					</form>
+				</div>
 			</div>
-			<div class="col-md-5">
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item active" aria-current="page"><a href="">Home</a></li>
-						<li class="breadcrumb-item active" aria-current="page"><a href="">Chuyên mục 1</a></li>
-						<li class="breadcrumb-item" aria-current="page">Bài viết <span class="badge badge-info">New</span></li>
-					</ol>
-				</nav>
-			</div>
-			<div class="col-md-3">
-				<button class="btn btn-success">OK</button>
+			<br>
+			<br>
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>STT</th>
+							<th>Work</th>
+							<th>Done</th>
+							<th>Date</th>
+							<th>Chú thích</th>
+						</tr>
+					</thead>
+					<tbody>
+					</tbody>
+				</table>
 			</div>
 		</div>
-		<div class="row main">
-			<div class="alert alert-warning alert-dismissible fade show" role="alert">
-				<strong>Holy guacamole!</strong> You should check in on some of those fields below.
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-		</div>
-	</div>
 
-
-
-	<script src="Bootstrap/js/jquery.js"></script>
-	<script src="Bootstrap/js/myJava.js"></script>
-	<script src="Bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
+		<script src="Bootstrap/js/jquery.js"></script>
+		<script src="Bootstrap/js/myJava.js"></script>
+		<script src="Bootstrap/js/bootstrap.min.js"></script>
+	</body>
+	</html>
 
 
--- index.php
+++ index.php
@@ -20,7 +20,5 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 
-
 ?>
-
 
